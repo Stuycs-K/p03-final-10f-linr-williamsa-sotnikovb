cee46083ec133cee52b9d11b250353fb50fb11e7
armisteadw <armisteadw@nycstudents.net>
2026-01-08 15:24:52 -0500

    fixed devlog and made small changes

--- server.c
+++ server.c
@@ -1,25 +1,8 @@
 #include "networking.h"
 
-char * rotX(char* n, int x){
-  for(int i = 0; i < strlen(n); i++){
-    if((*(n+i) >= 'a') && (*(n+i) <= 'z')){
-      *(n+i) = 'a' + ((*(n+i) + x - 'a') % 26);
-    }
-    if((*(n+i) >= 'A') && (*(n+i) <= 'Z')){
-      *(n+i) = 'Z' + ((*(n+i) + x - 'A') % 26);
-    }
-  }
-  return n;
-}
-
-void subserver_logic(int client_socket){
+void matchserverlogic(int client1, int client2){
   while(1){
-    char buff[BUFFER_SIZE];
-    if(0==recv(client_socket, buff, sizeof(buff), MSG_WAITALL)){
-      exit(0);
-    }
-    rotX(buff, 13);
-    send(client_socket, buff, sizeof(buff), 0);
+    #logictba
   }
 }
 
@@ -27,6 +10,13 @@ int main(int argc, char *argv[] ) {
   int listen_socket = server_setup();
 while(1){
   int client_socket = server_tcp_handshake(listen_socket);
+  fd_set desciptors;
+  
+
+
+
+
+
   int child = fork();
   if(!child){
     close(listen_socket);
