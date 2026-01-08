cfbe4e4a4d40bfa6d9c0df5d3566d888e3b12c7e
BogdanSotnikov <bogdans2789@nycstudents.net>
2026-01-08 14:02:40 -0600

    Devlog + working on client

--- DEVLOG-Bogdan.md
+++ DEVLOG-Bogdan.md
@@ -4,8 +4,5 @@ This document must be updated daily every time you finish a work session.
 
 ## Bogdan Sotnikov
 
-### 2024-01-02 - Brief description
-Expanded description including how much time was spent on task.
-
-### 2024-01-03 - Brief description
-Expanded description including how much time was spent on task.
+### 2024-01-07
+- Brainstormed networking and basic setup.
--- client.c
+++ client.c
@@ -1,8 +1,20 @@
 #include "networking.h"
 
+void clientLogic(int server_socket){
+  printf("Welcome to Battleship 3000\nPlease enter your username\n");
+  char buffer[256];
+  fgets()
+  while (1)
+  {
 
-int main(int argc, char const *argv[])
-{
-  printf("Welcome to Ultra Battleship III\nPlease enter your username\n");
-  return 0;
+  }
+}
+
+int main(int argc, char *argv[] ) {
+  char* IP = "127.0.0.1";
+  if(argc>1){
+    IP=argv[1];
+  }
+  int server_socket = client_tcp_handshake(IP);
+  clientLogic(server_socket);
 }
