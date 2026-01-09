892adb210772be1430111534ee65b390d47ffc1b
armisteadw <armisteadw@nycstudents.net>
2026-01-09 15:36:02 -0500

    Worked on select functionality

--- DEVLOG-Armistead.md
+++ DEVLOG-Armistead.md
@@ -9,3 +9,8 @@ Figured out project plan and distributed workload, assigned deadlines and segmen
 
 ### 2024-01-07 - Planning
 Figured out the exact systems-level interactions of our clients and servers. Took the entire period.
+
+### 2025-01-08 - Began work
+Began working on project, making a new server that can listening on multiple sockets simultaneously.
+
+### 2025-01-09 - Worked with select function
--- server.c
+++ server.c
@@ -1,4 +1,17 @@
 #include "networking.h"
+#include& lt; stdio.h & gt;
+#include& lt; string.h & gt; //strlen
+#include& lt; stdlib.h & gt;
+#include& lt; errno.h & gt;
+#include& lt; unistd.h & gt; //close
+#include& lt; arpa / inet.h & gt; //close
+#include& lt; sys / types.h & gt;
+#include& lt; sys / socket.h & gt;
+#include& lt; netinet / in.h & gt;
+#include& lt; sys / time.h & gt; //FD_SET, FD_ISSET, FD_ZERO macros
+#define TRUE 1
+#define FALSE 0
+#define PORT 6767
 
 void matchserverlogic(int client1, int client2){
   while(1){
@@ -6,9 +19,21 @@ void matchserverlogic(int client1, int client2){
   }
 }
 
+/*
+We will have a single main server that manages incoming connections, and manages match requests.
+We listen on an open, "master_socket", for new connections and listen on every other socket for match requests. This is achieved using the select function.
+When a client connects we add them to our select list and continue.
+When a client requests to play another client on the list, we must write to the other client socket, requiring us to have both reading and writing ports open.
+*/
 int main(int argc, char *argv[] ) {
-  int listen_socket = server_setup();
-  fd_set read_fds;
+  fd_set master;    // master file descriptor list
+  fd_set read_fds;  // temp file descriptor list for select()
+  int fdmax;        // maximum file descriptor number
+
+  int listener = server_setup();
+  FD_ZERO(&master);
+  FD_ZERO(&read_fds);
+  FD_SET
   int client_socket;
   char buffer[100];
 
@@ -17,9 +42,10 @@ int main(int argc, char *argv[] ) {
   listen_socket = server_setup();
 
   //add listen_socket and stdin to the set
-  FD_SET(listen_socket, &read_fds);
+  FD_SET(listen_socket, &master);
+  fdmax = listen_socket;
   //add stdin's file desciptor
-  FD_SET(STDIN_FILENO, &read_fds);
+  //FD_SET(STDIN_FILENO, &read_fds);
 
   //listen socket is larger than STDIN_FILENO, so listen_socket+1 is the 1 larger than max fd value.
   int i = select(listen_socket+1, &read_fds, NULL, NULL, NULL);
