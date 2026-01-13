91e9e750b37e56e27faf5a452d70a947cf12a80f
Ricky Lin <rlin61@stuy.edu>
2026-01-13 15:19:43 -0500

    server pritns

--- server.c
+++ server.c
@@ -54,6 +54,7 @@ void handle_new_connection(int listener, fd_set *master, int *fdmax){
     *fdmax = newfd;
     }
   }
+  printf("server connected");
 }
 
 void handle_client_data(int s, int listener, fd_set *master, int *fdmax){
@@ -76,6 +77,7 @@ void handle_client_data(int s, int listener, fd_set *master, int *fdmax){
     }
   }
   else{
+    printf("server receives %s", buffer);
     buf[nbytes] = '\0';
         // we got some data from a client
         // to implement how we handle this data
